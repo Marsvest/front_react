2719332f345e67cb5c92f4c9b7baef9e58d31b96
Marsvest <alajh.akbar@gmail.com>
2025-02-10 23:20:42 +1000

    store и импорты

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import './output.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import './output.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // Импортируем Provider
+import { store } from './store'; // Импортируем созданный store
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Оборачиваем App в Provider и передаем store */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
