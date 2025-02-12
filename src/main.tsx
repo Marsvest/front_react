b38561dac12fab94c08ddd860a4ebd4d28a10d31
Marsvest <alajh.akbar@gmail.com>
2025-02-12 23:40:53 +1000

    type del

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import './output.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux'; // Импортируем Provider
-import { store } from './store'; // Импортируем созданный store
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* Оборачиваем App в Provider и передаем store */}
     <Provider store={store}>
       <App />
     </Provider>
