b22db8436f25bd063b0509e71d90c6bb41822a76
Marsvest <alajh.akbar@gmail.com>
2025-02-15 20:13:23 +1000

    style: :bulb: delete comments

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,26 +1,23 @@
 import axios from 'axios';
-import { Project } from '../types/Project'; // Импортируем тип Project
+import { Project } from '../types/Project';
 
 const GITHUB_API_URL = 'https://api.github.com'
 
-// Функция для получения публичных репозиториев пользователя и преобразования их в тип Project
 export async function getPublicRepos(username: string): Promise<Project[]> {
     try {
-        // Выполняем запрос к GitHub API
         const response = await axios.get<any[]>(`${GITHUB_API_URL}/users/${username}/repos`, {
             params: {
-                type: 'public', // Получаем только публичные репозитории
+                type: 'public',
             },
         });
 
-        // Преобразуем данные GitHub API в массив проектов
         const projects: Project[] = response.data.map((repo) => ({
-            id: repo.id.toString(), // Преобразуем число в строку
-            title: repo.name, // Используем имя репозитория как заголовок
-            description: repo.description || 'Описание отсутствует', // Если описание null, используем fallback
-            technologies: repo.language ? [repo.language] : [], // Язык программирования как технология
-            link: repo.html_url, // Ссылка на репозиторий
-            category: 'Public Repository', // Категория (можете изменить на что-то более подходящее)
+            id: repo.id.toString(),
+            title: repo.name,
+            description: repo.description || 'Описание отсутствует',
+            technologies: repo.language ? [repo.language] : [],
+            link: repo.html_url,
+            category: 'Общая Категория',
         }));
 
         return projects;
